f72f2e727dbd18fa1828690e4282eb564969f1a3
anirudhyan <68052744+anirudhyan@users.noreply.github.com>
2022-04-09 17:50:24 +0530

    Create Python_Data_Validation_Script.py

---
+++ Python_Data_Validation_Script.py
@@ -0,0 +1,4 @@
+'''
+This Python script is used for Big data data Validation Purpose 
+Here we get the input as 2 excel files and they are the respective source and target data files , we are also getting the primary_key column of source and a dictionary called the mapping dictionary that has the source and target columns mapped as key value pair
+'''
